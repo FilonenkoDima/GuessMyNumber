3ac5dc7bc25235786e137b2be0090f0b9d7f23ed
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-01-23 18:07:22 +0200

    implement highscore

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -18,6 +19,11 @@ document.querySelector(".check").addEventListener("click", function () {
 
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
 
     // When is too high
   } else if (guess > secretNumber) {
